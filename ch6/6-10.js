ee707f605d62a33282b508e417ddc6fef3feddd2
taehee <jangth0655@gmail.com>
2022-11-25 10:49:55 +0900

    [refact] 캡슐화

--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,7 +1,28 @@
+/**
+ * 여러 함수를 변환 함수로 묶기
+ * 왠만하면 클래스를 이용하는 것이 좋다!
+ * 변경된 데이터가 업데이트되지 않는다.
+ */
+
+import _ from 'lodash';
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+// 얕은 복사만 이뤄진다.
+// 중첩된 객체가 있다면, 중첩된 객체까지는 새롭게 만들지 않는다.
+export function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseChange = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, this.baseCharge - this.taxThreshold);
+  return result;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
 }
 
 export function baseRate(month, year) {
--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,32 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+/**
+ * 객체는 항상 참조값으로 전달하기 때문에 변경될 수 있는 객체를 여기저기 전달하는 것 ❌
+ * 스프레드를 통해 객체를 반환하여 사용 -> 하지만 1단계 얕은 복사이기 때문에 위험할 수 있다.
+ * 따라서 ✅ 클래스를 사용하는 것을 추천!
+ */
+class Person {
+  #firstName;
+  #lastName;
+  constructor(firstName, lastName) {
+    (this.#firstName = firstName), this.#lastName, lastName;
+  }
+
+  // set가 없기 때문에 수정할 수 없다.
+  get lastName() {
+    return this.#lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: '마틴', lastName: '파울러' });
+
+/**
+ let defaultOwner = { firstName: '마틴', lastName: '파울러' };
 
 export function getDefaultOwner() {
-  return defaultOwner;
+  return { ...defaultOwner };
 }
+
+ */
