c3ae5183644505b87acd728af086364542e0ce56
taehee <jangth0655@gmail.com>
2022-11-27 10:44:30 +0900

    [refac] 필드와 함수 옮기기

--- ch8/8-1-1.js
+++ ch8/8-1-1.js
@@ -1,42 +1,24 @@
+/**
+ * 함수 내부에 쓰이고 있는 함수가 정말 trackSummary의 데이터와 밀접한 관련이 있는지
+ * 응집도가 높은 상태인지 확인
+ * 즉, 사용하는 데이터와 함수가 밀접하게 관련이 있는지
+ * calculateTime,calculateDistance은 계산된 데이터를 반환만 할 뿐
+ * 사용하는 데이터와는 연관이 없다.
+ */
+
 export function trackSummary(points) {
-  const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
-  const pace = totalTime / 60 / totalDistance;
+  const time = calculateTime();
+  const distance = calculateDistance(points);
+  const pace = time / 60 / distance;
   return {
-    time: totalTime,
-    distance: totalDistance,
-    pace: pace,
+    time,
+    distance,
+    pace,
   };
+}
 
-  function calculateDistance() {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-  }
-
-  function distance(p1, p2) {
-    // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
-    const EARTH_RADIUS = 3959; // in miles
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a =
-      Math.pow(Math.sin(dLat / 2), 2) +
-      Math.cos(radians(p2.lat)) *
-        Math.cos(radians(p1.lat)) *
-        Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-  }
-
-  function radians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
-
-  function calculateTime() {
-    return 10000;
-  }
+function calculateTime() {
+  return 10000;
 }
 
 const newYork = {
@@ -48,6 +30,32 @@ const tokyo = {
   lat: 35.652832,
   lon: 139.839478,
 };
+
+function distance(p1, p2) {
+  // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
+  const EARTH_RADIUS = 3959; // in miles
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.cos(radians(p2.lat)) *
+      Math.cos(radians(p1.lat)) *
+      Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return EARTH_RADIUS * c;
+}
+
+function radians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+function calculateDistance() {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+}
 
 const summary = trackSummary([newYork, tokyo]);
 console.log(summary);
--- ch8/8-2-1.js
+++ ch8/8-2-1.js
@@ -1,24 +1,23 @@
+/**
+ * 필드 옮기기
+ * 함수와 필드를 옮기는 것은 응집도를 높이기 위해서
+ */
+
 export class Customer {
   #name;
-  #discountRate;
   #contract;
   constructor(name, discountRate) {
     this.#name = name;
-    this.#discountRate = discountRate;
-    this.#contract = new CustomerContract(this.dateToday());
-  }
-
-  get discountRate() {
-    return this.#discountRate;
+    this.#contract = new CustomerContract(this.dateToday(), discountRate);
   }
 
   becomePreferred() {
-    this.#discountRate += 0.03;
+    this.#contract.discountRate += 0.03;
     // 다른 코드들이 있음...
   }
 
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
+    return amount.subtract(amount.multiply(this.#contract.discountRate));
   }
 
   dateToday() {
@@ -28,7 +27,17 @@ export class Customer {
 
 class CustomerContract {
   #startDate;
-  constructor(startDate) {
+  #discountRate;
+  constructor(startDate, discountRate) {
+    this.#discountRate = discountRate;
     this.#startDate = startDate;
+  }
+
+  get discountRate() {
+    return this.#discountRate;
+  }
+
+  set discountRate(value) {
+    this.#discountRate = value;
   }
 }
